74a6902913f02514bf0e7bd166d21d0bfd199612
Russell <halfbcsina@yahoo.com.sg>
2022-09-13 15:09:50 +0800

    initial commit

---
+++ miniprojserver/src/main/java/com/vttp/miniprojserver/MiniprojserverApplication.java
@@ -0,0 +1,13 @@
+package com.vttp.miniprojserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojserverApplication.class, args);
+	}
+
+}
